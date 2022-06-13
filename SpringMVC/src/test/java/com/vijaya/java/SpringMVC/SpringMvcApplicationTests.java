210a76c961e613036026d481db49c2743781cbd5
vijayakunwar <kunwar.vijaya@gmail.com>
2022-06-13 12:07:36 +1000

    spring boot starter project with web

---
+++ SpringMVC/src/test/java/com/vijaya/java/SpringMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaya.java.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
